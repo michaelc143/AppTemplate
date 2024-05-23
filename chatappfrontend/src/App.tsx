03d8d180f0ca60c79280e18f4265e2af98c14ca1
Michael Corbishley <mcorbishley@wisc.edu>
2024-05-23 12:04:14 -0500

    update routing

--- chatappfrontend/src/App.tsx
+++ chatappfrontend/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import home from './components/Home';
-import login from './components/Login';
-import register from './components/Register';
-import chatrooms from './components/Chatrooms';
-import pagenotfound from './components/PageNotFound';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Chatrooms from './components/Chatrooms';
+import PageNotFound from './components/PageNotFound';
 import { AuthProvider } from './contexts/AuthContext';
 import { ToastProvider } from './contexts/ToastContext';
 import { UserProvider } from './contexts/UserContext';
@@ -19,11 +19,11 @@ function App() {
           <ToastContainer />
           <Router>
             <Routes>
-              <Route path='/' Component={home} />
-              <Route path='/login' Component={login} />
-              <Route path='/register' Component={register} />
-              <Route path='/chatrooms' Component={chatrooms} />
-              <Route path='*' Component={pagenotfound} />
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/chatrooms' element={<Chatrooms />} />
+              <Route path='*' element={<PageNotFound />} />
             </Routes>
           </Router>
         </ToastProvider>
