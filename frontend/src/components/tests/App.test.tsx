d21f21e2f946582e21121bf0b0eba170dd583412
Michael Corbishley <mcorbishley@wisc.edu>
2024-05-26 17:46:33 -0500

    convert tabs to spaces

--- frontend/src/components/tests/App.test.tsx
+++ frontend/src/components/tests/App.test.tsx
@@ -4,46 +4,46 @@ import App from '../../App';
 describe('App', () => {
 
   test('renders welcome message', () => {
-		render(<App />);
+    render(<App />);
     const titleElement = screen.getByText(/Welcome to the app!/i);
     expect(titleElement).toBeInTheDocument();
   });
 
   test('renders login button', () => {
-		render(<App />);
+    render(<App />);
     const loginButton = screen.getByRole('link', { name: /Login/i });
     expect(loginButton).toBeInTheDocument();
   });
 
-	test('renders register button', () => {
-		render(<App />);
-		const registerButton = screen.getByRole('link', { name: /Register/i });
-		expect(registerButton).toBeInTheDocument();
-	});
+  test('renders register button', () => {
+    render(<App />);
+    const registerButton = screen.getByRole('link', { name: /Register/i });
+    expect(registerButton).toBeInTheDocument();
+  });
 
-	test('renders home button', () => {
-		render(<App />);
-		const homeButton = screen.getByRole('link', { name: /Home/i });
-		expect(homeButton).toBeInTheDocument();
-	});
+  test('renders home button', () => {
+    render(<App />);
+    const homeButton = screen.getByRole('link', { name: /Home/i });
+    expect(homeButton).toBeInTheDocument();
+  });
 });
 
 describe('Nav', () => {
 
   test('login button navs to /login', () => {
-		render(<App />);
+    render(<App />);
     const loginButton = screen.getByRole('link', { name: /Login/i });
-		fireEvent.click(loginButton);
-		expect(window.location.pathname).toBe('/login');
-		const homeButton = screen.getByRole('link', { name: /Back to Home/i }); // have to nav back to home to run register test
-		fireEvent.click(homeButton);
+    fireEvent.click(loginButton);
+    expect(window.location.pathname).toBe('/login');
+    const homeButton = screen.getByRole('link', { name: /Back to Home/i }); // have to nav back to home to run register test
+    fireEvent.click(homeButton);
   });
 
-	test('register button navs to /register', () => {
-		render(<App />);
+  test('register button navs to /register', () => {
+    render(<App />);
     const registerButton = screen.getByRole('link', { name: /Register/i });
-		fireEvent.click(registerButton);
-		expect(window.location.pathname).toBe('/register');
+    fireEvent.click(registerButton);
+    expect(window.location.pathname).toBe('/register');
   });
 
 });
