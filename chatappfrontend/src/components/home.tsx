04418f68e9528fd6288cefd9334be16acff724b0
Michael Corbishley <mcorbishley@wisc.edu>
2024-05-26 14:26:40 -0500

    spacing fixes

--- chatappfrontend/src/components/home.tsx
+++ chatappfrontend/src/components/home.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function home() {
   return (
-	<div className="flex flex-col items-center justify-center my-16">
-		<h1 className="mb-8 font-bold text-4xl">Welcome to the chat app!</h1>
-	</div>
+    <div className="flex flex-col items-center justify-center my-16">
+      <h1 className="mb-8 font-bold text-4xl">Welcome to the chat app!</h1>
+    </div>
   )
 }
