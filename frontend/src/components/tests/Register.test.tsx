a245b7947b284c2c02f5fafa38b26c889da03c38
Michael Corbishley <mcorbishley@wisc.edu>
2024-05-29 20:18:22 -0500

    add submit form test

--- frontend/src/components/tests/Register.test.tsx
+++ frontend/src/components/tests/Register.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 import { UserContext } from '../../contexts/UserContext';
@@ -7,7 +7,6 @@ import Register from '../Register';
 
 describe('Register', () => {
   test('renders Register form and allows typing', () => {
-
     render(
       <Router>
         <AuthContext.Provider value={{ isLoggedIn: false }}>
@@ -27,6 +26,45 @@ describe('Register', () => {
     fireEvent.change(usernameInput, { target: { value: 'testuser' } });
     fireEvent.change(emailInput, { target: { value: 'testuser@example.com' } });
     fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+
+    expect(usernameInput.value).toBe('testuser');
+    expect(emailInput.value).toBe('testuser@example.com');
+    expect(passwordInput.value).toBe('testpassword');
+  });
+
+  it('submits the form with username, email and password', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve(
+        new Response(JSON.stringify({ success: true }))
+      )
+    );
+
+    render(
+      <Router>
+        <AuthContext.Provider value={{ isLoggedIn: false }}>
+          <UserContext.Provider value={{ user: {} }}>
+            <ToastContext.Provider value={{ showToast: jest.fn() }}>
+              <Register />
+            </ToastContext.Provider>
+          </UserContext.Provider>
+        </AuthContext.Provider>
+      </Router>
+    );
+
+    const usernameInput = screen.getByLabelText('Username') as HTMLInputElement;
+    const emailInput = screen.getByLabelText('Email') as HTMLInputElement;
+    const passwordInput = screen.getByLabelText('Password') as HTMLInputElement;
+    const submitButton = screen.getByRole('button', { name: /Register/i }) as HTMLButtonElement;
+
+    fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+    fireEvent.change(emailInput, { target: { value: 'testuser@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(global.fetch).toHaveBeenCalledTimes(1));
+
+    expect(global.fetch).toHaveBeenCalledWith("http://localhost:5000/api/register",
+    {"body": "{\"username\":\"testuser\",\"email\":\"testuser@example.com\",\"password\":\"testpassword\"}", "headers": {"Content-Type": "application/json"}, "method": "POST"});
 
     expect(usernameInput.value).toBe('testuser');
     expect(emailInput.value).toBe('testuser@example.com');
